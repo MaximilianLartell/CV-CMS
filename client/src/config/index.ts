c3d88782221362c0d8b90bc6e6f6c8f13703d9c7
Maximilian Skoglund Lartell <max.lartell@gmail.com>
2021-06-27 21:38:28 +0200

    fixed breaking changes

--- client/src/config/index.ts
+++
@@ -1,6 +0,0 @@
-const localPath = 'http://localhost:8000';
-const externalPath = 'https://maximilian-reminder.herokuapp.com';
-
-const PATH = process.env.NODE_ENV === 'production' ? externalPath : localPath;
-
-export default PATH;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import Store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={Store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
